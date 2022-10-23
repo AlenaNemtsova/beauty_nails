4e672e94348bf844419edc7d7627640fc9a590d8
Daniya <daniyashagieva@gmail.com>
2022-10-23 19:34:38 +0300

    services day1

---
+++ index.js
@@ -0,0 +1,305 @@
+//JSON для комбинированных услуг
+//Маникюр + покрытие
+const combinedServiceManicureTop = [
+    {
+    name: 'Маникюр классический / комбинированный / аппаратный, покрытие гель-лак',
+    price: 1700,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр бразильский, покрытие гель-лак',
+    price: 1900,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр мужской, покрытие гель-лак',
+    price: 1900,
+    currency: '₽'
+    }
+]
+
+//Маникюр + покрытие + снятие
+
+const combinedServiceManicureTopRemove = [
+    {
+    name: 'Маникюр классический / комбинированный / аппаратный + покрытие гель-лак + снятие',
+    price: 1900,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр Бразильский + Покрытие гель-лак + Снятие',
+    price: 2100,
+    currency: '₽'
+    }
+]
+
+//Маникюр + покрытие + выравнивание
+
+const combinedServiceManicureTopAlignment = [
+    {
+    name: 'Маникюр классический / комбинированный / аппаратный + покрытие гель-лак + выравнивание',
+    price: 2300,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр + Выравнивание + Покрытие гель-лак Luxio',
+    price: 2500,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр Бразильский + Выравнивание + Покрытие гель-лак',
+    price: 2500,
+    currency: '₽'
+    }
+]
+
+//Маникюр + покрытие + выравнивание + снятие
+
+const combinedServiceManicureTopAlignmentRemove = [
+    {
+    name: 'Маникюр классический/ комбинированный/ аппаратный + выравнивание + покрытие гель-лак + снятие',
+    price: 2400,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр Бразильский + Выравнивание + Покрытие гель-лак + Снятие',
+    price: 2700,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр Бразильский + Выравнивание + Покрытие гель-лак Luxio + Снятие',
+    price: 2900,
+    currency: '₽'
+    }
+]
+
+//Маникюр + укрепление ногтевой пластины
+
+const combinedServiceManicureStrengthening = [
+    {
+    name: 'Маникюр Классический/ Комбинированный/ Аппаратный + Укрепление ногтевой пластины IBX/ Vitagel/ Masura',
+    price: 1200,
+    currency: '₽'
+    },
+    {
+    name: 'Маникюр Бразильский + Укрепление ногтевой пластины IBX/ Vitagel/ Masura',
+    price: 1500,
+    currency: '₽'
+    },
+    {
+    name: 'Выравнивание ногтевой пластины базой',
+    price: 600,
+    currency: '₽'
+    }
+]
+
+//Педикюр + покрытие
+
+const combinedServicePedicureTop = [
+    {
+    name: 'Педикюр классический/ комбинированный/ аппаратный + покрытие гель-лак',
+    price: 2500,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр классический/ комбинированный/ аппаратный + покрытие гель-лак Luxio',
+    price: 2900,
+    currency: '₽'
+    },
+    {
+    name: 'Глубокий эстетический педикюр Golden Trace + Покрытие гель-лак',
+    price: 3200,
+    currency: '₽'
+    },
+    {
+    name: 'Экспресс педикюр (только пальчики) + покрытие гель-лак',
+    price: 2100,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр бразильский + покрытие гель-лак',
+    price: 2600,
+    currency: '₽'
+    },
+    {
+    name: 'Смарт педикюр с покрытием гель-лак',
+    price: 2800,
+    currency: '₽'
+    }
+]
+
+//Педикюр + покрытие + снятие
+
+const combinedServicePedicureTopRemove = [
+    {
+    name: 'Педикюр классический/ комбинированный/ аппаратный + покрытие гель-лак + снятие',
+    price: 2700,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр Комбинированный + Гель-Лак + Снятие Гель-Лак',
+    price: 2600,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр Бразильский + Покрытие Гель-Лак + Снятие',
+    price: 2700,
+    currency: '₽'
+    },
+    {
+    name: 'Экспресс педикюр (только пальчики) + покрытие гель-лак + снятие',
+    price: 2300,
+    currency: '₽'
+    },
+    {
+    name: 'Снятие + Смарт педикюр + Покрытие гель-лак',
+    price: 3000,
+    currency: '₽'
+    }
+]
+
+//Педикюр + укрепление ногтевой пластины
+
+const combinedServicePedicureStrengthening = [
+    {
+    name: 'Педикюр Классический/ Комбинированный + Укрепление ногтевой пластины IBX/ Vitagel/ Masura',
+    price: 1900,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр Аппаратный + Укрепление ногтевой пластины IBX/ Vitagel/ Masura',
+    price: 1900,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр Аппаратный + Укрепление ногтевой пластины IBX/ Vitagel/ Masura + Снятие',
+    price: 2200,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр Бразильский + Укрепление ногтевой пластины IBX/ Vitagel/ Masur',
+    price: 2100,
+    currency: '₽'
+    },
+    {
+    name: 'Педикюр Бразильский + Укрепление ногтевой пластины IBX/ Vitagel/ Masura + Снятие',
+    price: 2400,
+    currency: '₽'
+    },
+    {
+    name: 'Экспресс Педикюр (только пальчики) + Укрепление ногтевой пластины IBX/ Vitagel/ Masura',
+    price: 1600,
+    currency: '₽'
+    }
+]
+
+//контейнер для карточек комбинированных услуг Маникюр + Покрытие
+const combinedServiceListManicureTop = document.querySelector('.service__list_manicure-top');
+
+//контейнер для карточек комбинированных услуг Маникюр + Покрытие + Снятие
+const combinedServiceListManicureTopRemove = document.querySelector('.service__list_manicure-top-remove');
+
+//контейнер для карточек комбинированных услуг Маникюр + Покрытие + Выравнивание
+const combinedServiceListManicureTopAlignment = document.querySelector('.service__list_manicure-top-alignment');
+
+//контейнер для карточек комбинированных услуг Маникюр + Покрытие + Выравнивание
+const combinedServiceListManicureTopAlignmentRemove = document.querySelector('.service__list_manicure-top-alignment-remove');
+
+//контейнер для карточек комбинированных услуг Маникюр + Укрепление ногтевой пластины
+const combinedServiceListManicureStrengthening = document.querySelector('.service__list_manicure-strengthening');
+
+//контейнер для карточек комбинированных услуг Педикюр + покрытие
+const combinedServiceListPedicureTop = document.querySelector('.service__list_pedicure-top');
+
+//контейнер для карточек комбинированных услуг Педикюр + покрытие + снятие
+const combinedServiceListPedicureTopRemove = document.querySelector('.service__list_pedicure-top-remove');
+
+//контейнер для карточек комбинированных услуг Педикюр + укрепление ногтевой пластины
+const combinedServiceListPedicureStrengthening = document.querySelector('.service__list_pedicure-strengthening');
+
+//создаем карточки
+const createCard = (obj) => {
+    const block = document.createElement('div');
+    block.className = 'card__item';
+    const name = document.createElement('span');
+    name.className = 'card__name';
+    name.textContent = obj.name;
+    const price = document.createElement('span');
+    price.className = 'card__price';
+    price.textContent = obj.price;
+    const currency = document.createElement('span');
+    currency.className = 'card__currency';
+    currency.textContent = obj.currency;
+    const button = document.createElement('button');
+    button.className = 'card__btn';
+    button.textContent = 'Записаться';
+
+    block.append(name);
+    block.append(price);
+    block.append(currency);
+    block.append(button);
+    //console.log(block);
+    return block;
+}
+
+//функция для создания карточки комбинированных услуг
+const addCard = (objItem, container) => {
+    const item = createCard(objItem);
+    container.append(item);
+}
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Маникюр + Покрытие
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServiceManicureTop.forEach((element) => {
+            addCard(element, combinedServiceListManicureTop);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Маникюр + Покрытие + Снятие
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServiceManicureTopRemove.forEach((element) => {
+            addCard(element, combinedServiceListManicureTopRemove);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Маникюр + Покрытие + Выравнивание
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServiceManicureTopAlignment.forEach((element) => {
+            addCard(element, combinedServiceListManicureTopAlignment);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Маникюр + Покрытие + Выравнивание + Снятие
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServiceManicureTopAlignmentRemove.forEach((element) => {
+            addCard(element, combinedServiceListManicureTopAlignmentRemove);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Маникюр + Укрепление ногтевой пластины
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServiceManicureStrengthening.forEach((element) => {
+            addCard(element, combinedServiceListManicureStrengthening);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Педикюр + Покрытие
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServicePedicureTop.forEach((element) => {
+            addCard(element, combinedServiceListPedicureTop);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Педикюр + Покрытие + Снятие
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServicePedicureTopRemove.forEach((element) => {
+            addCard(element, combinedServiceListPedicureTopRemove);
+        })
+    })
+
+    //тут циклом проходимся по JSON-у,  чтобы отрисовать каждую карточку для комбинированных услуг Педикюр + Укрепление ногтевой пластины
+    document.addEventListener('DOMContentLoaded', () => {
+        combinedServicePedicureStrengthening.forEach((element) => {
+            addCard(element, combinedServiceListPedicureStrengthening);
+        })
+    })
